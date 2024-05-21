c6b9f3eb8f018669281a87dc2269a0ff2631e193
sathya11042000 <sathyastudy2000@gmail.com>
2024-05-21 12:15:17 +0530

    anu done her work

--- src/org/sample/GitProject.java
+++ src/org/sample/GitProject.java
@@ -1,10 +1,14 @@
 package org.sample;
 
 public class GitProject {
-	
+
 	public static void main(String[] args) {
-		String s="welcome to git";
+		String s = "welcome to git";
 		System.out.println(s);
+
+		int a = 34;
+		System.out.println(a);
+
 	}
 
 }
